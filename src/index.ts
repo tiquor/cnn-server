6395e6ffa0453c1fbb76032f0a965af0f4ef1f4b
Jochizan <remnyachizot2015@gmail.com>
2021-10-26 16:21:07 -0500

    Finish project

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,11 @@ import config from "./config";
 
 const app = express();
 
-app.use(cors({}));
+app.use(
+  cors({
+    origin: "https://tiquor.github.io"
+  })
+);
 
 app.use("/v1/", express.static("cnn"));
 
